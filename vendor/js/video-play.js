c5d6b4bc96456e0f5271a1e56a3db48868700a53
rinarexhepi <rinnaarexhepi@gmail.com>
2023-04-11 18:08:57 +0200

    updated js function

--- vendor/js/video-play.js
+++ vendor/js/video-play.js
@@ -1,10 +1,11 @@
 function playVideo() {
     var modal = document.getElementById("signup-popup");
     var video = document.getElementById("info-video");
-    if (video.style.display === "hidden") {
+    if (video.style.display === "none" && modal.style.display === "none") {
         video.style.display = "block";
+        modal.style.display = "none"
     }
-    if (modal.style.display === "block") {
-        modal.style.display = "none";
-    }
+    console.log('test')
   }
+
+  
