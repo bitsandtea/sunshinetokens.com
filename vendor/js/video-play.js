a51a74769743061c043fccb7d29f54740701659d
rinarexhepi <rinnaarexhepi@gmail.com>
2023-04-16 20:17:50 +0200

    Video shows after clicking submit now button

--- vendor/js/video-play.js
+++ vendor/js/video-play.js
@@ -1,12 +1,18 @@
-function playVideo(event) {
-    event.preventDefault();
+const submitButton = document.getElementById("quote_submit_btn");
+
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  console.log("Submit button clicked!");
+  // add your custom code here to handle the button click event
+});
+
+function playVideo() {
     var modal = document.getElementById("signup-popup");
     var video = document.getElementById("info-video");
-    if (video.style.display === "none" && modal.style.display === "none") {
-        video.style.display = "block";
-        modal.style.display = "none"
-    }
+    modal.style.display = "none";
+    video.style.display = "block"
     console.log('test')
   }
+
 
   
